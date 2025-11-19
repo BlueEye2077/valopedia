e48552e2bafe842a125d91dcff4512319b913c8b
Blue Eye <ab3062004@gmail.com>
2025-11-20 00:04:53 +0200

    Enhancing Agent model

--- lib/data/models/agent.dart
+++ lib/data/models/agent.dart
@@ -29,136 +29,35 @@ class Agent {
   List<dynamic>? abilities;
 
   Agent.fromJson(Map<String, dynamic> json) {
-    uuid = json["uuid"];
-    displayName = json["displayName"];
-    description = json["description"];
-    developerName = json["developerName"];
-    releaseDate = json["releaseDate"];
-    characterTags = json["characterTags"];
+    uuid = json["uuid"] as String?;
+    displayName = json["displayName"] as String?;
+    description = json["description"] as String?;
+    developerName = json["developerName"] as String?;
+    releaseDate = json["releaseDate"] as String?;
+    characterTags = json["characterTags"] as List<dynamic>?;
 
-    displayIcon = json["displayIcon"];
-    displayIconSmall = json["displayIconSmall"];
-    bustPortrait = json["bustPortrait"];
-    fullPortrait = json["fullPortrait"];
-    fullPortraitV2 = json["fullPortraitV2"];
-    killfeedPortrait = json["killfeedPortrait"];
-    minimapPortrait = json["minimapPortrait"];
-    homeScreenPromoTileImage = json["homeScreenPromoTileImage"];
-    background = json["background"];
+    displayIcon = json["displayIcon"] as String?;
+    displayIconSmall = json["displayIconSmall"] as String?;
+    bustPortrait = json["bustPortrait"] as String?;
+    fullPortrait = json["fullPortrait"] as String?;
+    fullPortraitV2 = json["fullPortraitV2"] as String?;
+    killfeedPortrait = json["killfeedPortrait"] as String?;
+    minimapPortrait = json["minimapPortrait"] as String?;
+    homeScreenPromoTileImage = json["homeScreenPromoTileImage"] as String?;
+    background = json["background"] as String?;
 
-    backgroundGradientColors = json["backgroundGradientColors"];
+    backgroundGradientColors =
+        json["backgroundGradientColors"] as List<dynamic>?;
 
-    assetPath = json["assetPath"];
+    assetPath = json["assetPath"] as String?;
 
-    isFullPortraitRightFacing = json["isFullPortraitRightFacing"];
-    isPlayableCharacter = json["isPlayableCharacter"];
-    isAvailableForTest = json["isAvailableForTest"];
-    isBaseContent = json["isBaseContent"];
+    isFullPortraitRightFacing = json["isFullPortraitRightFacing"] as bool?;
+    isPlayableCharacter = json["isPlayableCharacter"] as bool?;
+    isAvailableForTest = json["isAvailableForTest"] as bool?;
+    isBaseContent = json["isBaseContent"] as bool?;
 
-    role = json["role"];
+    role = json["role"] as Map<String, dynamic>?;
 
-    abilities = json["abilities"];
+    abilities = json["abilities"] as List<dynamic>?;
   }
 }
-
-
-
-
-        // {
-        //     "uuid": "e370fa57-4757-3604-3648-499e1f642d3f",
-        
-        //     "displayName": "Gekko",
-        
-        //     "description": "Gekko the Angeleno leads a tight-knit crew of calamitous creatures. His buddies bound forward, scattering enemies out of the way, with Gekko chasing them down to regroup and go again.",
-        
-        //     "developerName": "Aggrobot",
-        
-        //     "releaseDate": "1970-01-01T00:00:00Z",
-        
-        //     "characterTags": null,
-        
-        //=================================================================================== 
-        
-        //     "displayIcon": "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/displayicon.png",
-        
-        //     "displayIconSmall": "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/displayicon.png",
-        
-        //     "bustPortrait": "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/fullportrait.png",
-        
-        //     "fullPortrait": "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/fullportrait.png",
-        
-        //     "fullPortraitV2": "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/fullportrait.png",
-        
-        //     "killfeedPortrait": "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/killfeedportrait.png",
-        
-        //     "minimapPortrait": "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/minimapportrait.png",
-        
-        //     "homeScreenPromoTileImage": null,
-        
-        //     "background": "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/background.png",
-        
-// ================================================================================================
-
-        //     "backgroundGradientColors": [
-        //         "c7f458ff",
-        //         "d56324ff",
-        //         "3a2656ff",
-        //         "3a7233ff"
-        //     ],
-// ================================================================================================
-        
-        //     "assetPath": "ShooterGame/Content/Characters/AggroBot/AggroBot_PrimaryAsset",
-        
-        //     "isFullPortraitRightFacing": false,
-        
-        //     "isPlayableCharacter": true,
-        
-        //     "isAvailableForTest": false,
-        
-        //     "isBaseContent": false,
-// ================================================================================================
-        
-        //     "role": {
-        //         "uuid": "1b47567f-8f7b-444b-aae3-b0c634622d10",
-        //         "displayName": "Initiator",
-        //         "description": "Initiators challenge angles by setting up their team to enter contested ground and push defenders away.",
-        //         "displayIcon": "https://media.valorant-api.com/agents/roles/1b47567f-8f7b-444b-aae3-b0c634622d10/displayicon.png",
-        //         "assetPath": "ShooterGame/Content/Characters/_Core/Roles/Breaker_PrimaryDataAsset"
-        //     },
-        
-        // =====================================================
-        //    todo "recruitmentData": null,
-        // =====================================================
-        // 
-        //     "abilities": [
-        //         {
-        //             "slot": "Ability1",
-        //             "displayName": "Wingman",
-        //             "description": "EQUIP Wingman. FIRE to send Wingman forward seeking enemies. Wingman unleashes a concussive blast toward the first enemy he sees. ALT FIRE when targeting a Spike site or planted Spike to have Wingman defuse or plant the Spike. To plant, Gekko must have the Spike in his inventory. When Wingman expires he reverts into a dormant globule. INTERACT to reclaim the globule and gain another Wingman charge after a short cooldown.",
-        //             "displayIcon": "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/abilities/ability1/displayicon.png"
-        //         },
-        
-        //         {
-        //             "slot": "Ability2",
-        //             "displayName": "Dizzy",
-        //             "description": "EQUIP Dizzy. FIRE to send Dizzy soaring forward through the air. Dizzy charges then unleashes plasma blasts at enemies in line of sight. Enemies hit by her plasma are Blinded. When Dizzy expires she reverts into a dormant globule. INTERACT to reclaim the globule and gain another Dizzy charge after a short cooldown.",
-        //             "displayIcon": "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/abilities/ability2/displayicon.png"
-        //         },
-        
-        //         {
-        //             "slot": "Grenade",
-        //             "displayName": "Mosh Pit",
-        //             "description": "EQUIP Mosh. FIRE to throw Mosh like a grenade. ALT FIRE to lob. Upon landing Mosh duplicates across a large area that deals a small amount of damage over time then after a short delay explodes.",
-        //             "displayIcon": "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/abilities/grenade/displayicon.png"
-        //         },
-        
-        //         {
-        //             "slot": "Ultimate",
-        //             "displayName": "Thrash",
-        //             "description": "EQUIP Thrash. FIRE to link with Thrash’s mind and steer her through enemy territory. ACTIVATE to lunge forward and explode, Detaining any players in a small radius. When Thrash expires she reverts into a dormant globule. INTERACT to reclaim the globule and gain another Thrash charge after a short cooldown. Thrash can be reclaimed once.",
-        //             "displayIcon": "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/abilities/ultimate/displayicon.png"
-        //         }
-        //     ],
-        // =====================================================
-        //     todo "voiceLine": null
-        // },
